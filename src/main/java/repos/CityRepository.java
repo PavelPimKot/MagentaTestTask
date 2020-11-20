493c140b713d035aafe244177bf4a1c5f3c208d6
PavelPimKot <57503765+PavelPimKot@users.noreply.github.com>
2020-11-20 20:04:16 +0400

    Repository classes( for working with table in database) added

---
+++ src/main/java/repos/CityRepository.java
@@ -0,0 +1,7 @@
+package repos;
+
+import infoClasses.City;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CityRepository  extends CrudRepository<City, Integer> {
+}
---
+++ src/main/java/repos/DistanceRepository.java
@@ -0,0 +1,7 @@
+package repos;
+
+import infoClasses.Distance;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DistanceRepository  extends CrudRepository<Distance, Integer> {
+}
